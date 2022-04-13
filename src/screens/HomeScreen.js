54abc91e1f1c1488da25321f45f41a71d14512e5
NotPerr <83740861+NotPerr@users.noreply.github.com>
2022-04-13 14:16:37 +0800

    change account navigation (#2)

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Image,VStack,HStack,Text ,ScrollView,View,NativeBaseProvider,Center,Pressable,FlatList,StatusBar} from "native-base"
+import { Box, Image,VStack,HStack,Text ,ScrollView,View,NativeBaseProvider,Center,Pressable,FlatList,StatusBar,ColorMode, useColorMode} from "native-base"
 import { TouchableOpacity } from "react-native";
 
 import booksData from "../json/books.json";
@@ -9,27 +9,41 @@ import NewList from "../components/NewList";
 
 
 const HomeScreen = ({ navigation ,books}) => {
-    
+    const{ColorMode} = useColorMode();
     return(
         <NativeBaseProvider>
             <StatusBar/>
-            <ScrollView alwaysBounceVertical="false">
-            <VStack >
-            <Center size={16} >
-                每日推薦
-            </Center>
-            <HStack>
-               
-                <Image source={{
-                uri: "https://raw.githubusercontent.com/NotPerr/app-mid/main/assets/Mockup.png"
-                }} alt="Alternate Text" w="133" />
-                <Center w="171">
-                    <Text>羅曼斯-經典中的愛情</Text>
-                    <Text>822位讀者推薦</Text>
-                    <Text>感情沒有對錯，也不存在值不值得。從來都是你情我願的事，
-                        而你被傷得體無完膚也怨不了任何人，是你自己心甘情願給你愛的人一把刀子，被捅得血流如注，你還是會咬着牙忍着痛去靠近他。</Text>
-                </Center>
-            </HStack>
+            <ScrollView alwaysBounceVertical="false"
+                _light = {{ bg:"#6868AC"}}
+                _dark = {{ bg:"#353552"}}
+                
+            >
+            <VStack 
+         
+            
+            >
+                <VStack
+                  
+                 >
+                    <Center size={16} 
+                         
+                    >
+                        每日推薦
+                    </Center>
+                    <HStack >
+                    
+                        <Image source={{
+                        uri: "https://raw.githubusercontent.com/NotPerr/app-mid/main/assets/Mockup.png"
+                        }} alt="Alternate Text" w="133" />
+                        <Center w="171">
+                            <Text>羅曼斯-經典中的愛情</Text>
+                            <Text>822位讀者推薦</Text>
+                            <Text>感情沒有對錯，也不存在值不值得。從來都是你情我願的事，
+                                而你被傷得體無完膚也怨不了任何人，是你自己心甘情願給你愛的人一把刀子，被捅得血流如注，你還是會咬着牙忍着痛去靠近他。</Text>
+                        </Center>
+                    </HStack>
+                </VStack>
+            
 
             <Text bg="#C2B6B3"></Text>
             <Text bg="#E4DAD8"></Text>
