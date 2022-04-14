ea91abd5476b4d2ea8b20aadef4fcc2256a50161
laiqi <a21954262@gmail.com>
2022-04-14 10:28:15 +0800

    adjust

--- src/components/HeaderR.js
+++ src/components/HeaderR.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Box } from "native-base"
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { render } from "react-dom";
 
 export default class AccountHeaderR extends Component{
     render(){
